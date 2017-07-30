977fd8e0aa281ca6b7d50f017a05e42c7d8c1739
Richard Nguyen <nguyenrichn@gmail.com>
2017-07-30 14:20:40 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Novery.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        // GET: api/home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
