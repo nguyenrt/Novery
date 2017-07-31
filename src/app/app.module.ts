23d1af7b1346981621ba8c2b514882c8e58ec3c0
Richard Nguyen <nguyenrichn@gmail.com>
2017-07-31 00:35:22 -0400

    Fixed issue that occurred on first time compilation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { AppConfig } from './config/app.config';
 
-function initConfig(config: AppConfig) {
+export function initConfig(config: AppConfig) {
   return () => config.load();
 }
 
